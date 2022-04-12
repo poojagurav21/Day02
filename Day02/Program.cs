8ee81d39dcd507698ae159e7af765b3692171c26
Pooja Gurav <pooja.gurav@waiin.com>
2022-04-12 18:18:49 +0530

    Add project files.

---
+++ Day02/Program.cs
@@ -0,0 +1,155 @@
+﻿//while(true)
+//{ 
+//    Console.WriteLine("hello");
+//}
+//int a = 1;
+//while (a <= 20)
+//{
+//    string res = a % 2 == 0 ? "= even" : "= odd";
+//    Console.WriteLine(a + res);
+//    a++;
+//}
+
+//int num = 0;
+//while(num<=100)
+//{
+//    if (num % 5 == 0 && num % 2 == 0)
+//    {
+//        Console.WriteLine(num);
+
+//    }
+
+//    num+=10;
+//}
+
+//prime number 
+//Console.WriteLine("Enter number: ");
+//int num=int.Parse(Console.ReadLine());
+//bool flag = true;
+//int i = 2;
+//while(i<num/2)
+//{
+//    if(num%i==0)
+//    {
+//        flag = false;
+//        break;
+//    } 
+//    i++;
+//}
+//if (flag)
+//    Console.WriteLine(num +" prime number");
+//else
+//    Console.WriteLine(num+" not Prime number");
+
+//for(int i=0;i<=10;i++)
+//{
+//    Console.WriteLine("hello");
+//}
+//Console.WriteLine("Odd Numbers between 1 to 100");
+//for(int i=0;i<=100;i++)
+//{
+//   if(i%2!=0)
+//        Console.WriteLine(i);
+//}
+
+//Console.WriteLine("Divisible by 5 and 2");
+//for (int i = 1; i <= 100; i++)
+//{
+//    if (i % 5 == 0 && i%2==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+
+////for (int i = 1; i <= 3; i++)
+////{
+////    for (int j = 1; j <= i; j++)
+////    {
+////        Console.Write(j + "\t");
+////    }
+////    Console.WriteLine();
+////}
+
+
+//for(int i=1;i<=3;i++)
+//{
+//    for(int j = 1; j <= 4; j++)
+//    {
+
+//        if(i==2)
+//        {
+//            Console.Write("2\t");
+//        }
+//        else
+//            Console.Write("0\t");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//Console.WriteLine("Enter row number: ");
+//int rows=int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter column number: ");
+//int cols=int.Parse(Console.ReadLine());
+//for (int i = 1; i <= rows; i++)
+//{
+//    for (int j = 1; j <= cols; j++)
+//    {
+
+//        if (i==j)
+//        {
+
+//                Console.Write("1\t");
+//        }
+//       else
+//            Console.Write("0\t");
+//    }
+//    Console.WriteLine();
+//}
+
+
+Console.WriteLine("Enter row number: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter column number: ");
+int cols = int.Parse(Console.ReadLine());
+for (int i = 1; i <= rows; i++)
+{
+    for (int j = 1; j <= cols; j++)
+    {
+
+        if (i == j)
+        {
+
+            Console.Write("1\t");
+        }
+        else
+            Console.Write("0\t");
+        if (i == cols)
+        {
+            Console.Write("1\t");
+        }
+        else
+            Console.Write("0\t");
+    }
+    Console.WriteLine();
+}
+
+
+
+//Console.WriteLine("Enter row number: ");
+//int rows = int.Parse(Console.ReadLine());
+//int a = 1;
+//for (int i = 1; i <= rows; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+
+//        Console.Write(a + "\t");
+//        a++;
+
+//    }
+//    Console.WriteLine();
+//}
+
---
+++ array/Program.cs
@@ -0,0 +1,204 @@
+﻿//int[] arr = new int[5];
+//arr[0] = 13;
+//arr[1] = 14;
+//arr[2] = 15;
+//arr[3] = 16;
+//arr[4] = 17;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+
+
+//int[] arr = new int[5];
+//Console.WriteLine("Enter 5 values: ");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    arr[i] = int.Parse(Console.ReadLine());
+//}
+//Console.WriteLine("Entered value of array are: ");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+
+
+//dynamic size declaration
+
+//Console.WriteLine("Enter size of array: ");
+//int size=int.Parse(Console.ReadLine());
+//int[] arr=new int[size];
+//Console.WriteLine("Enter "+size+" values: ");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    arr[i] = int.Parse(Console.ReadLine());
+//}
+//Console.WriteLine("Entered value of array are: ");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+
+//Console.WriteLine("Enter values: ");
+//int[,] arr = new int[3, 3];
+//for(int i=0;i<3;i++)
+//{
+//    for(int j=0;j<3;j++)
+//    {
+
+//        arr[i,j] = int.Parse(Console.ReadLine());
+//    }
+//}
+
+//Console.WriteLine("entered values are: ");
+//for (int i = 0; i < 3; i++)
+//{
+//    for (int j = 0; j < 3; j++)
+//    {
+//        Console.Write(arr[i,j]+"\t");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//Console.WriteLine("Enter values: ");
+//int[,] arr = new int[3, 4];
+//for(int i = 0; i < 3; i++)
+//{
+//    for(int j = 0; j < 4; j++)
+//    {
+//        arr[i, j] = int.Parse(Console.ReadLine());
+//    }
+//}
+//for(int i = 0; i < 3; i++)
+//{
+//    for(int j=0; j < 4; j++)
+//    {
+//        if(arr[i, j] %5==0)
+//        {
+//            Console.Write("*\t");
+//        }
+//        else
+//        {
+//            Console.Write(arr[i, j]+"\t");
+//        }
+//    }
+//    Console.WriteLine();
+//}
+
+//for (int i = 1; i <= 3; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(j + "\t");
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("Enter rows and column value: ");
+//int rows=int.Parse(Console.ReadLine());
+//int cols=int.Parse(Console.ReadLine());
+//Console.WriteLine("entered values are: ");
+//int[,] arr = new int[rows, cols];
+//int a = 0;
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < cols; j++)
+//    {
+//        if(j<i)
+//            Console.Write("\t");
+//        else
+//            Console.Write("*\t");
+
+//    }
+//    Console.WriteLine();
+//}
+
+
+//Console.WriteLine("Enter rows number: ");
+//int rows=int.Parse(Console.ReadLine());
+//int cols = rows;
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < cols; j++)
+//    {
+//        if (i == 0 || i == rows - 1 || j == 0 || j == rows - 1)
+//            Console.Write("*\t");       
+//        else
+//            Console.Write("\t");
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("Enter row number: ");
+//int row = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < row; i++)
+//{
+//    for (int j = 0; j < row; j++)
+//    {
+//        if (j == 0 || i == row - 1)
+//            Console.Write("*\t");
+//        else
+//            Console.Write("\t");
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("Enter row number: ");
+//int row = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < row; i++)
+//{
+//    for (int j = 0; j < row; j++)
+//    {
+//        if (i == row - 1 || j == row - 1)
+//            Console.Write("*\t");
+//        else
+//            Console.Write("\t");
+//    }
+//    Console.WriteLine();
+//}
+Console.WriteLine("Enter row number: ");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter row number: ");
+int col = int.Parse(Console.ReadLine());
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+        if ((i == 0 || i == (row / 2) || j == 0  )||(j==col-1)&&i<(row/2))
+            Console.Write("*\t");
+        else
+            Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
